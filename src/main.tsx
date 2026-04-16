f938ed651e5d604df638cac83de52f5b675c58ef
hani <gim325901@gmail.com>
2026-04-16 17:51:37 +0900

    feat: 메인 라우터 설정 및 tailwind css 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "@pages/Home";
+import { Work } from "@pages/Work";
+import { RootLayout } from "@layouts/RootLayout";
 import "./index.css";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/work", element: <Work /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
 );
