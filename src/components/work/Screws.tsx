71836ba1b175b6e98d175883c0fe53e488d8bd6f
hani <gim325901@gmail.com>
2026-05-11 13:41:19 +0900

    refactor: screws 공통 컴포넌트로 수정

---
+++ src/components/work/Screws.tsx
@@ -0,0 +1,10 @@
+export function Screws() {
+  return (
+    <>
+      <div className="absolute top-2 left-2 h-2 w-2 rounded-full bg-[#3B3B44]" />
+      <div className="absolute top-2 right-2 h-2 w-2 rounded-full bg-[#3B3B44]" />
+      <div className="absolute bottom-2 left-2 h-2 w-2 rounded-full bg-[#3B3B44]" />
+      <div className="absolute right-2 bottom-2 h-2 w-2 rounded-full bg-[#3B3B44]" />
+    </>
+  );
+}
