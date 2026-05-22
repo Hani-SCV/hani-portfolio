357f0f7fd939eca17efaddee49c2278a1ace5006
hani <gim325901@gmail.com>
2026-05-22 13:55:39 +0900

    feat: 하드 디스크 캐릭터 구현

---
+++ src/components/work/HardDisk.tsx
@@ -0,0 +1,34 @@
+type Props = {
+  leftPupilRef?: React.RefObject<HTMLDivElement | null>;
+  rightPupilRef?: React.RefObject<HTMLDivElement | null>;
+};
+
+export function HardDisk({ leftPupilRef, rightPupilRef }: Props) {
+  return (
+    <>
+      <div className="relative mx-auto h-70 w-80">
+        <img
+          src="/characters/hard-disk/hard-disk.png"
+          className="h-full w-full object-contain"
+        />
+
+        <div className="pointer-events-none absolute inset-0 top-1 left-26 w-25">
+          <img src="/characters/hard-disk/hard-disk-eya.png" />
+
+          <svg
+            className="absolute inset-0"
+            viewBox="0 0 400 250"
+            xmlns="http://www.w3.org/2000/svg"
+          ></svg>
+          <div ref={leftPupilRef} className="absolute top-8 left-3">
+            <img src="/characters/gameboy/gameboy-pupil.svg" className="w-10" />
+          </div>
+
+          <div ref={rightPupilRef} className="absolute top-8 left-10">
+            <img src="/characters/gameboy/gameboy-pupil.svg" className="w-10" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
