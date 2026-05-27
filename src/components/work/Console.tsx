1d8016df08f63cc579fef0551cf94d52ef5b4232
hani <gim325901@gmail.com>
2026-05-27 18:25:30 +0900

    refactor: [콘솔] 이미지 변경

--- src/components/work/Console.tsx
+++ src/components/work/Console.tsx
@@ -6,27 +6,25 @@ type Props = {
 export function Console({ leftPupilRef, rightPupilRef }: Props) {
   return (
     <>
-      <div className="relative mx-auto h-70 w-80">
-        <img
-          src="/characters/console/console.png"
-          className="h-full w-full object-contain"
-        />
+      <img
+        src="/characters/console/console2.png"
+        className="h-full w-full object-contain"
+      />
 
-        <div className="pointer-events-none absolute inset-0 top-3 left-27 w-25">
-          <img src="/characters/hard-disk/hard-disk-eya.png" />
+      <div className="pointer-events-none absolute inset-0 top-13 left-27 w-25">
+        <img src="/characters/hard-disk/hard-disk-eya.png" />
 
-          <svg
-            className="absolute inset-0"
-            viewBox="0 0 400 250"
-            xmlns="http://www.w3.org/2000/svg"
-          ></svg>
-          <div ref={leftPupilRef} className="absolute top-8 left-3">
-            <img src="/characters/gameboy/gameboy-pupil.svg" className="w-10" />
-          </div>
+        <svg
+          className="absolute inset-0"
+          viewBox="0 0 400 250"
+          xmlns="http://www.w3.org/2000/svg"
+        ></svg>
+        <div ref={leftPupilRef} className="absolute top-8 left-3">
+          <img src="/characters/gameboy/gameboy-pupil.svg" className="w-10" />
+        </div>
 
-          <div ref={rightPupilRef} className="absolute top-8 left-10">
-            <img src="/characters/gameboy/gameboy-pupil.svg" className="w-10" />
-          </div>
+        <div ref={rightPupilRef} className="absolute top-8 left-10">
+          <img src="/characters/gameboy/gameboy-pupil.svg" className="w-10" />
         </div>
       </div>
     </>
