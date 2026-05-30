39da40eecb85e2f66a694417f36962e348ddef7f
hani <gim325901@gmail.com>
2026-05-30 20:32:41 +0900

    feat: [콘솔] 캐릭터 색상 별 이미지 구현

--- src/components/work/Console.tsx
+++ src/components/work/Console.tsx
@@ -1,18 +1,21 @@
+import { useCustomizerStore } from "@/shared/stores/useCustomizerStore";
+
 type Props = {
   leftPupilRef?: React.RefObject<HTMLDivElement | null>;
   rightPupilRef?: React.RefObject<HTMLDivElement | null>;
 };
 
 export function Console({ leftPupilRef, rightPupilRef }: Props) {
+  const color = useCustomizerStore((s) => s.color);
   return (
     <>
       <img
-        src="/characters/console/console.png"
+        src={`/characters/console/console-${color.name}.png`}
         className="h-full w-full object-contain"
       />
 
       <div className="pointer-events-none absolute inset-0 top-13 left-27 w-25">
-        <img src="/characters/hard-disk/hard-disk-eya.png" />
+        <img src="/characters/hardDisk/hardDisk-eya.png" />
 
         <svg
           className="absolute inset-0"
