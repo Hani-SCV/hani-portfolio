2b9952e556563c3d28e980650f43f1f69fb73e9a
hani <gim325901@gmail.com>
2026-07-29 20:37:38 +0900

    chore: lint 최적화를 위한 lint-staged 추가 및 import 통일 lib 추가 및 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import react from 'eslint-plugin-react'
-import prettier from 'eslint-config-prettier'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist', 'node_modules']),
+  globalIgnores(["dist", "node_modules"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       react.configs.flat.recommended,
-      react.configs.flat['jsx-runtime'],
+      react.configs.flat["jsx-runtime"],
     ],
-    languageOptions: { 
+    languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    settings: { react: { version: 'detect' } },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
   },
 
-  prettier
-])
+  prettier,
+]);
