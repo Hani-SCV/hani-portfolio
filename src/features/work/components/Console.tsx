d9833d7f305fa0684b445440bb58a50a11dc91ce
hani <gim325901@gmail.com>
2026-07-30 16:37:11 +0900

    refactor: 콘솔 컴포넌트 구독 범위 최적화 및 오타 수정

--- src/features/work/components/Console.tsx
+++ src/features/work/components/Console.tsx
@@ -6,22 +6,17 @@ type Props = {
 };
 
 export function Console({ leftPupilRef, rightPupilRef }: Props) {
-  const color = useCustomizerStore((s) => s.color);
+  const colorName = useCustomizerStore((s) => s.color.name);
   return (
     <>
       <img
-        src={`/characters/console/console-${color.name}.png`}
+        src={`/characters/console/console-${colorName}.png`}
         className="h-full w-full object-contain"
       />
 
       <div className="pointer-events-none absolute inset-0 top-13 left-27 w-25">
-        <img src="/characters/hardDisk/hardDisk-eya.png" />
+        <img src="/characters/hardDisk/hardDisk-eye.png" />
 
-        <svg
-          className="absolute inset-0"
-          viewBox="0 0 400 250"
-          xmlns="http://www.w3.org/2000/svg"
-        ></svg>
         <div ref={leftPupilRef} className="absolute top-8 left-3">
           <img src="/characters/gameboy/gameboy-pupil.svg" className="w-10" />
         </div>
