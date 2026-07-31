07dab3f89c27cbcb4c5e2ddaea2fad28452b32aa
hani <gim325901@gmail.com>
2026-07-31 13:17:50 +0900

    refactor: 캐릭터 컴포넌트 오타 수정 및 구조분해 개선

--- src/features/work/components/Gameboy.tsx
+++ src/features/work/components/Gameboy.tsx
@@ -6,11 +6,11 @@ type Props = {
 };
 
 export function Gameboy({ leftPupilRef, rightPupilRef }: Props) {
-  const color = useCustomizerStore((s) => s.color);
+  const colorName = useCustomizerStore((s) => s.color.name);
   return (
     <>
       <img
-        src={`/characters/gameboy/gameboy-${color.name}.png`}
+        src={`/characters/gameboy/gameboy-${colorName}.png`}
         className="h-full w-full object-contain"
       />
 
